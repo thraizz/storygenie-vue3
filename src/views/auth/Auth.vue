772690554685630c3344bfd1a09da90d39d0c50f
Aron Schüler <aron.schueler@ambient.digital>
2024-06-12 09:08:38 +0200

    fix: correct button styles in Auth views

--- src/views/auth/Auth.vue
+++ src/views/auth/Auth.vue
@@ -25,7 +25,7 @@ const tabs = computed(() => [
       <select
         id="tabs"
         name="tabs"
-        class="block w-full rounded-md border-gray-300 py-2 pl-3 pr-10 text-base focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+        class="block w-full rounded-md border-2 border-gray-300 py-2 pl-3 pr-10 text-base focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
         @change="(event: any) => $router.push(event.target?.value)"
       >
         <option
